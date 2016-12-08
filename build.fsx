28734aebb4ffea577ada37137377ddd3535918c4
James Goodwin <jamesgoodwin@gmail.com>
2016-12-08 14:47:54 +0000

    Add fake build script for Teamcity to build Xamarin Sample App

--- build.fsx
+++ build.fsx
@@ -1,73 +1,48 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.XamarinHelper
-open System.IO
 
-//Build Directories:
-let pclDir = "src/JudoDotNetXamarin/bin/Debug"
-let iOSSDKDir = "src/JudoDotNetXamariniOSSDK/bin/Debug"
-let droidSDKDir = "src/JudoDotNetXamarinAndroidSDK/bin/Debug"
+let buildDir = "samples/Samples/bin/Debug"
 
-let intTestDir = "src/JudoDotNetXamariniOS.Tests/bin/Debug"
-let uiTestDir = "src/JudoXamarin.UiTests/bin/Debug"
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
 
-let packageDir = "pack/"
-let outputDir = "build/"
+Target "Test" (fun _ ->
+    trace "Testing stuff..."
+)
 
-//Restore Nuget Packages and Clean
-Target "Restore Packages" (fun _ ->
+Target "Build-Pcl" (fun _ ->
     RestorePackages()
+
+    !! "samples/Samples/Samples.csproj"
+        |> MSBuild "samples/Samples/bin/Debug" "Build"  [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] 
+        |> Log "---PCL build output---"
 )
 
-//Build SDKs
-Target "Build SDK" (fun _ ->
-    !! "src/JudoDotNetXamarin/JudoDotNetXamarin.csproj"
-        |> MSBuild "src/JudoDotNetXamarin/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
-        |> Log "---Core build output...---"
-)
-
-Target "Build iOS SDK" (fun _ ->
-    iOSBuild (fun defaults ->
-        {
-            defaults with ProjectPath = "src/JudoDotNetXamariniOSSDK/JudoXamarin.iOS.csproj"
-                          OutputPath = "src/JudoDotNetXamariniOSSDK/bin/Debug"
-                          BuildIpa = false
-                          Configuration = "Debug"
+Target "Build-iOS" (fun _ ->
+    iOSBuild (fun defaults -> 
+        { 
+            defaults with ProjectPath = "samples/iOS/Samples.iOS.csproj"
+                          OutputPath = "samples/iOS/bin/iPhoneSimulator/Debug"
+                          Configuration = "Debug|iPhoneSimulator"
                           Target = "Build"
-        }) 
+        })
 )
 
-Target "Build Droid SDK" (fun _ ->
-    !! "src/JudoDotNetXamarinAndroidSDK/JudoDotNetXamarinAndroidSDK.csproj"
-        |> MSBuild "src/JudoDotNetXamarinAndroidSDK/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
+Target "Build-Droid" (fun _ ->
+    !! "samples/Droid/Samples.Droid.csproj"
+        |> MSBuild "samples/Droid/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
         |> Log "----Android build output----"
 )
 
-//Build Test Projects
-Target "Build Integration Test App" (fun _ -> 
-    iOSBuild (fun defaults ->
-        {
-            defaults with ProjectPath = "src/JudoDotNetXamariniOS.Tests/JudoXamarin.IntegrationTests.csproj"
-                          OutputPath = "src/JudoDotNetXamariniOS.Tests/bin/Debug"
-                          BuildIpa = true
-                          Configuration = "Debug"
-                          Target = "Build"
-        }) 
-)
+"Clean"
+  ==> "Build-Pcl"
+  ==> "Test"
 
-Target "Build UI Tests" (fun _ ->
-    !! "JudoXamarin.UiTests/JudoXamarin.UiTests.csproj"
-        |> MSBuild "src/JudoXamarin.UiTests/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
-        |> Log "----UI Test build output----"
-)
+"Clean"
+  ==> "Build-Pcl"
+  ==> "Build-iOS"
+  ==> "Build-Droid"
 
-
-//Build Order
-"Restore Packages"    
-    ==> "Build SDK"    
-    ==> "Build iOS SDK"
-    ==> "Build Droid SDK"
-    ==> "Build Integration Test App"
-    ==> "Build UI Tests"
-
-RunTargetOrDefault "Build UI Tests"
+RunTargetOrDefault "Test"
