c18980c4eff0e446ea1e9e703d57872a964e6c66
Zeno Foltin <zeno.foltin@gmail.com>
2019-04-22 20:06:53 +0100

    Simplify sample, remove unnecessary fluff

--- samples/Droid/MainActivity.cs
+++ samples/Droid/MainActivity.cs
@@ -1,48 +1,43 @@
 ﻿using System;
-
 using Android.App;
-using Android.Views;
+using Android.Content.PM;
 using Android.OS;
 using Java.Interop;
 using SampleApp.Helpers;
-using Android.Content.PM;
 
 namespace Samples.Droid
 {
-	[Activity(Label = "Judopay", Icon = "@drawable/ic_launcher", Theme = "@style/JudoTheme", ScreenOrientation = ScreenOrientation.Locked, MainLauncher = true)]
-	public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar;
+    [Activity(Label = "Judopay", Icon = "@drawable/ic_launcher", Theme = "@style/JudoTheme", ScreenOrientation = ScreenOrientation.Locked, MainLauncher = true)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate(bundle);
-			Xamarin.Forms.Forms.Init(this, bundle);
+            base.OnCreate(savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
-			Window.SetSoftInputMode(SoftInput.AdjustResize);
-	
-			LoadApplication(new App());
-		}
+            LoadApplication(new App());
+        }
 
-		[Export("SetSetting")]
-		public string SetSetting(string setting)
-		{
-			switch (setting)
-			{
-				case "Maestro":
-					Settings.MaestroAllowed = true;
-					break;
-				case "Amex":
-					Settings.AmexAllowed = true;
-					break;
-				case "AVS":
-					Settings.AvsEnabled = true;
-					break;
-				default:
-					throw new ArgumentException(setting + " is not a recognised setting");
-			}
-			return setting;
-		}
-	}
+        [Export("SetSetting")]
+        public string SetSetting(string setting)
+        {
+            switch (setting)
+            {
+                case "Maestro":
+                    Settings.MaestroAllowed = true;
+                    break;
+                case "Amex":
+                    Settings.AmexAllowed = true;
+                    break;
+                case "AVS":
+                    Settings.AvsEnabled = true;
+                    break;
+                default:
+                    throw new ArgumentException(setting + " is not a recognised setting");
+            }
+            return setting;
+        }
+    }
 }
