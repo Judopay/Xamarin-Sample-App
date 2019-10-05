0a0d380ab38197c6a426e7fd3650eb9c968c7629
Zeno Foltin <zeno.foltin@gmail.com>
2019-10-05 20:23:09 +0100

    Update dependencies

--- samples/iOS/Main.cs
+++ samples/iOS/Main.cs
@@ -7,6 +7,10 @@ namespace Samples.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            // workaround for https://github.com/xamarin/xamarin-macios/issues/6654
+            using (var _ = new AddressBook.ABGroup())
+            {
+            }
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
